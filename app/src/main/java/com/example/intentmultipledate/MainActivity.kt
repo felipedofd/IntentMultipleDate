a9cce10555665e6b3ebb42d08530503cb96441a1
felipedofd <[Dfelipedofd@hotmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[[C>
2022-08-04 16:47:12 -0300

    create a intents to share data into activities.

--- app/src/main/java/com/example/intentmultipledate/MainActivity.kt
+++ app/src/main/java/com/example/intentmultipledate/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.intentmultipledate
 
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.intentmultipledate.databinding.ActivityMainBinding
@@ -12,10 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val imageView = R.drawable.image
+        val name = "24kGold"
+        val description = getString(R.string.description_24kgold)
 
         binding.imageView.setOnClickListener {
-            val intent = Intent(this, SeccondActivity::class.java)
-            intent.putExtra("ImageView")
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("image", imageView)
+            intent.putExtra("description", description)
+            intent.putExtra("name", name)
+
+            startActivity(intent)
         }
 
     }
